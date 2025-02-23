bc78ef523f760ac1fdbab4ad1bf41677149c8f30
Gyurkin <admin@yui.sk>
2025-02-23 18:40:28 +0100

    Add custom Vite dev server config for DDEV integration
    Expose Vite development server ports in DDEV and update Vite config for proper origin handling and secure CORS setup. This ensures compatibility with DDEV's environment and allows seamless local development with proper URL and domain handling.

--- vite.config.mjs
+++ vite.config.mjs
@@ -43,5 +43,19 @@ export default defineConfig(({command}) => ({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
+    // Defines the origin of the generated asset URLs during development, this must be set to the
+    // Vite dev server URL and selected port. In general, `process.env.DDEV_PRIMARY_URL` will give
+    // us the primary URL of the DDEV project, e.g. "https://test-vite.ddev.site". But since DDEV
+    // can be configured to use another port (via `router_https_port`), the output can also be
+    // "https://test-vite.ddev.site:1234". Therefore we need to strip a port number like ":1234"
+    // before adding Vites port to achieve the desired output of "https://test-vite.ddev.site:3000".
+    origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, "")}:3000`,
+
+    // 👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+    // Configure CORS securely for the Vite dev server to allow requests from *.ddev.site domains,
+    // supports additional hostnames (via regex). If you use another `project_tld`, adjust this.
+    cors: {
+      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
   },
 }));
